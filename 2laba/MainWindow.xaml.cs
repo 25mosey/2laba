22853565b5e4036849662d7c78f6607a03d536c8
nikm1 <nikm1@NO1MORE>
2022-05-27 16:23:25 +0200

    Добавьте файлы проекта.

---
+++ 2laba/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Books2
+{
+    public enum Theme
+    {
+        Economic, Economic1, Economic2, Economic3, Math , Math1, Math2, Math3, Historia, Historia1 , Historia2, Historia3, Russian, Russian1, Russian2, Russian3
+    }
+    public partial class MainWindow : Window
+    {
+        Themes facultet = new Themes();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }  
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Catalog.Text = "Format: Test, theme." + '\n';
+            for (int i = 0; i < 6; i++)
+            {
+                Catalog.Text += facultet.Name_Students[i] + ", " + facultet.Theme_str[i] + ", " + facultet.Theme_str1[i] + ", " + facultet.Theme_str2[i] + ", " + facultet.Theme_str3[i] + "." + '\n';
+            }
+        }
+
+    }
+}
---
+++ 2laba/Questions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books2
+{
+    public class Themes
+    {
+        public int n;
+        public string[] Name_Students;
+        public string[] Student = { "Набор 1", "Набор 2", "Набор 3", "Набор 4", "Набор 5" , "Набор 6", };
+        public string[] Theme_str;
+        public string[] Theme_str1;
+        public string[] Theme_str2;
+        public string[] Theme_str3;
+        public Theme facultet;
+        public Random random;
+        public Themes()
+        {
+            n = 6;
+            Name_Students = new string[n];
+            Theme_str = new string[n];
+            Theme_str1 = new string[n];
+            Theme_str2 = new string[n];
+            Theme_str3 = new string[n];
+            random = new Random();
+            int k = 0;
+            for (int i = 0; i < Student.Length; i++)
+            {
+                for (int j = k; j < k + 1; j++)
+                {
+                    Name_Students[j] = Student[i];
+                }
+                k += 1;
+            }
+            int themename;
+            for (int i = 0; i < Theme_str.Length; i++)
+            {
+                themename = random.Next(16);
+                Theme theme = (Theme)themename;
+                Theme_str[i] = Convert.ToString(theme);
+            }
+            for (int i = 0; i < Theme_str.Length; i++)
+            {
+                themename = random.Next(16);
+                Theme theme = (Theme)themename;
+                Theme_str1[i] = Convert.ToString(theme);
+            }
+            for (int i = 0; i < Theme_str.Length; i++)
+            {
+                themename = random.Next(16);
+                Theme theme = (Theme)themename;
+                Theme_str2[i] = Convert.ToString(theme);
+            }
+            for (int i = 0; i < Theme_str.Length; i++)
+            {
+                themename = random.Next(16);
+                Theme theme = (Theme)themename;
+                Theme_str3[i] = Convert.ToString(theme);
+            }
+        }
+    }
+}
